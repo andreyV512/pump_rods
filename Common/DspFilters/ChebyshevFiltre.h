e249b8c8e7cf51086980580d87656b5e2a49cc1a
andrew <andrey@uran-ndt.ru>
2019-01-17 16:38:24 +0500

    dsp

--- Common/DspFilters/ChebyshevFiltre.h
+++ Common/DspFilters/ChebyshevFiltre.h
@@ -7,7 +7,7 @@ class LowFiltre
 	char implementation_buffer[760];
 public:
 	LowFiltre();
-	//.Setup(4000, 3, 50, 40); частота дискретизации, порядок фильтра, частота среза, затухание
+	//f.Setup(4000, 3, 50, 40); частота дискретизации, порядок фильтра, частота среза, затухание
 	void Setup(int sample_rate, int order, double cutoffFrequency, double stopBandDb);
 	double operator()(double value);
 };
@@ -19,8 +19,7 @@ class HighFiltre
 	char implementation_buffer[760];
 public:
 	HighFiltre();
-	//.Setup(4000, 3, 50, 40); частота дискретизации, порядок фильтра, частота среза, затухание
+	//f.Setup(4000, 3, 50, 40); частота дискретизации, порядок фильтра, частота среза, затухание
 	void Setup(int sample_rate, int order, double cutoffFrequency, double stopBandDb);
 	double operator()(double value);
 };
-
