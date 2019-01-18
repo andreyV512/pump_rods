1f32135a205703278b82e971b4a9a79da186a8da
andrew <andrey@uran-ndt.ru>
2019-01-18 11:00:09 +0500

    cebishev

--- Common/DspFilters/ChebyshevFiltre.h
+++ Common/DspFilters/ChebyshevFiltre.h
@@ -1,12 +1,13 @@
 #pragma once
 #include "include/ChebyshevII.h"
 
-template<class T>class DSPFiltre: public T
+template<class T>class DSPFiltre: public T::State<Dsp::DirectFormII>
 {
+	T m_design;
 public:
-	double operator()(double value)
+	inline double operator()(double value)
 	{
-		return m_state.m_state[0].process(value, typename T::m_design);
+		return process(value, m_design);
 	}
 
 	void Setup(int sample_rate, int order, double cutoffFrequency, double stopBandDb)
@@ -16,9 +17,9 @@ public:
 		params[1] = order;
 		params[2] = cutoffFrequency;
 		params[3] = stopBandDb;
-		setParams (params);
+		m_design.setParams (params);
 	}
 };
 
-class LowFiltre : public DSPFiltre<Dsp::FilterDesign<Dsp::ChebyshevII::Design::LowPass<5>, 1>>{};
-class HighFiltre: public DSPFiltre<Dsp::FilterDesign<Dsp::ChebyshevII::Design::HighPass<5>, 1>>{};
+class LowFiltre : public DSPFiltre<Dsp::ChebyshevII::Design::LowPass<5>>{};
+class HighFiltre: public DSPFiltre<Dsp::ChebyshevII::Design::HighPass<5>>{};
