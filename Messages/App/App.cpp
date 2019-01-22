802594a9b0705e208e4fc267952e11666655e622
andrew <andrey@uran-ndt.ru>
2019-01-22 10:30:01 +0500

    просмотр событий - полупрозрачное окно top_most

--- Messages/App/App.cpp
+++ Messages/App/App.cpp
@@ -12,13 +12,20 @@ namespace{
 
 App::App()
 {
-    RECT r;
+	RECT r;
 	WindowPosition::Get<MainWindow>(r);
-	HWND h = WindowTemplate(&mainWindow, L"События", r.left, r.top, r.right, r.bottom, IDI_LETTER);
+	WindowClass<MainWindow> t;
+	MyRegisterClass(Viewer<MainWindow>::Proc, t(),  CS_HREDRAW | CS_VREDRAW | CS_DBLCLKS
+		, IDI_LETTER, IDI_LETTER, NULL);
+
+	HWND h = CreateWindowEx(WS_EX_CLIENTEDGE | WS_EX_TOPMOST | WS_EX_LAYERED, t(), L"События", WS_OVERLAPPEDWINDOW, r.left, r.top, r.right, r.bottom, NULL, NULL, (HINSTANCE)::GetModuleHandle(NULL), &mainWindow); 
+
 	DWORD dwStyle = GetWindowLong(h, GWL_STYLE);
 	dwStyle &= ~(WS_MAXIMIZEBOX);
 	SetWindowLong(h, GWL_STYLE, dwStyle);
+
 	ShowWindow(h, SW_SHOWNORMAL);
+	SetLayeredWindowAttributes (h, 0, (255*70)/100, LWA_ALPHA);
 }
 
 void App::Destroy()
