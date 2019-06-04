a52536e729371371b5f52c57dae3b4dfeb4fe240
John Doe <jdoe@email.com>
2019-06-04 14:27:47 +0500

    xxx

--- convert/convert.cpp
+++ convert/convert.cpp
@@ -5,6 +5,8 @@
 #include <stdio.h>
 #include <Windows.h>
 
+#pragma warning(disable : 4996)
+
 unsigned FileSize(FILE *f)
 {
 	fseek(f,0L,SEEK_END);
@@ -13,8 +15,7 @@ unsigned FileSize(FILE *f)
 	return size;
 }
 
-
-int _tmain(int argc, _TCHAR* argv[])
+void Conv()
 {
 	FILE *fD = fopen("ac test3.vlt", "rb");
 	FILE *fA = fopen("dc test.vlt", "rb");
@@ -32,7 +33,7 @@ int _tmain(int argc, _TCHAR* argv[])
 		fwrite(&magic_number, sizeof(magic_number), 1, fR);
 
 		fwrite(&counterA, 2 * sizeof(counterA), 1, fR);
-		
+
 		while(!feof(fA))
 		{
 			if(0 == fread(data, sizeof(data), 1, fA))break;
@@ -40,7 +41,7 @@ int _tmain(int argc, _TCHAR* argv[])
 			fwrite(&res, sizeof(res), 1, fR);
 			++counterA;
 		}
-		
+
 		while(!feof(fD))
 		{
 			if(0 == fread(data, sizeof(data), 1, fD))break;
@@ -48,24 +49,6 @@ int _tmain(int argc, _TCHAR* argv[])
 			fwrite(&res, sizeof(res), 1, fR);
 			++counterB;
 		}
-
-		//while(!feof(fA))
-		//{
-		//	long long x;
-		//	if(0 == fread(&x, sizeof(x), 1, fA))break;
-		////	res = 10.0 * data[1];
-		////	fwrite(&res, sizeof(res), 1, fR);
-		//	++counterA;
-		//}
-		//
-		//while(!feof(fD))
-		//{
-		//	long long x;
-		//	if(0 == fread(&x, sizeof(x), 1, fD))break;
-		//	//res = 10.0 * data[0];
-		//	//fwrite(&res, sizeof(res), 1, fR);
-		//	++counterB;
-		//}
 
 		fseek(fR,sizeof(int),SEEK_SET);
 		fwrite(&counterA, sizeof(counterA), 1, fR);
@@ -77,6 +60,68 @@ int _tmain(int argc, _TCHAR* argv[])
 
 		printf("%d %d\n", counterA, counterB);
 	}
+}
+
+double def[6000000];
+double str[6000000];
+double res[6000000];
+
+void Ch()
+{
+	FILE *f= fopen("D:\\Projects\\Work\\pump_rods\\convert\\test3.dat", "rb");
+	if(NULL == f)
+	{
+		printf("err open file");
+		//	getchar();
+		return;
+	}
+	int magicNumber;
+	fread(&magicNumber, 1, sizeof(int), f);
+
+	int defCount;
+	fread(&defCount, sizeof(int), 1, f);
+
+	int strCount;
+	fread(&strCount, sizeof(int), 1, f);
+
+	fread(def, defCount, sizeof(double), f);
+	fread(str, strCount, sizeof(double), f);
+
+	fclose(f);
+
+
+	int count = 243450 - 123950;
+	double delta = 441500.0 / count;
+
+	int pred = 0;
+
+	for(int i = 123950; i < 123950 + count; ++i)
+	{
+		int last = int(delta * (1 + i - 123950));
+		for(int j = pred; j < last; ++j)
+		{
+			res[j] = str[i];
+		}
+		pred = last;
+	}
+
+	f= fopen("D:\\Projects\\Work\\pump_rods\\convert\\test4.dat", "wb+");
+	fwrite(&magicNumber  , sizeof(magicNumber), 1, f);
+
+	fwrite(&defCount  , sizeof(defCount), 1, f);
+	fwrite(&strCount  , sizeof(strCount), 1, f);
+
+	fwrite(def   , sizeof(double), defCount, f);
+	fwrite(res   , sizeof(double), strCount, f);
+	fclose(f);
+	printf("ok\n");
+}
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	//Conv();
+	Ch();
 	getchar();
 	return 0;
 }
