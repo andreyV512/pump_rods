b57cfc8d6cf2adeb15b4cfc29e2ed8c44bebcea9
actay <andrey@uran-ndt.ru>
2019-08-05 09:08:49 +0500

    Config

--- dstart.bat
+++ dstart.bat
@@ -1,3 +1,4 @@
 call "C:\Progra~2\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
 msbuild pump_rods.sln /t:pump_rods /p:Configuration=Debug
+msbuild pump_rods.sln /t:Messages /p:Configuration=Debug
 pause
