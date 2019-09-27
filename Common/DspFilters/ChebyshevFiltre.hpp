8a6ffdbe2f6bf6ad6ad303ebb49934814fe6bca0
andreyv512 <jdoe@email.com>
2019-09-27 13:39:23 +0500

    chebishev f

--- Common/DspFilters/ChebyshevFiltre.hpp
+++ Common/DspFilters/ChebyshevFiltre.hpp
@@ -9,20 +9,14 @@ public:
 	{
 		return process(value, m_design);
 	}
-
 	void Setup(int sample_rate, double cutoffFrequency, double stopBandDb)
 	{	
-		Dsp::Params params;
-		params[0] = sample_rate;
-		params[1] = 3;
-		params[2] = cutoffFrequency;
-		params[3] = stopBandDb;
-		m_design.setParams (params);
+		m_design.setup(3, sample_rate, cutoffFrequency, stopBandDb);
 	}
 };
 
-class LowFiltre : public DSPFiltre<Dsp::ChebyshevII::Design::LowPass<3>>{};
-class HighFiltre: public DSPFiltre<Dsp::ChebyshevII::Design::HighPass<3>>{};
+class LowFiltre : public DSPFiltre<Dsp::ChebyshevII::LowPass<3>>{};
+class HighFiltre: public DSPFiltre<Dsp::ChebyshevII::HighPass<3>>{};
 
 class BandPassFiltre: public Dsp::ChebyshevII::BandPass<3>::State<Dsp::DirectFormII>
 {
@@ -35,5 +29,20 @@ public:
 	void Setup(int sample_rate, int widthFrequency, double centerFrequency, double stopBandDb)
 	{
 		m_design.setup(3, sample_rate, centerFrequency, widthFrequency, 0.01);
+	}
+};
+
+class DoubleFiltre
+{
+public:
+	class O{};
+	O *o;
+	double (O::*ptr)(double );
+public:
+	LowFiltre lowFiltre;
+	BandPassFiltre bandPassFiltre;
+	inline double operator()(double value)
+	{
+		return (o->*ptr)(value);
 	}
 };
