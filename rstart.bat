f22ee534429151ca79df945636e5c3b94ec6380b
A <nobody@mail.net>
2020-10-06 10:29:41 +0500

    okok

--- rstart.bat
+++ rstart.bat
@@ -1,3 +1,5 @@
-call "C:\Progra~1\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
+call "C:\Progra~2\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
 msbuild pump_rods.sln /t:pump_rods /p:Configuration=Release
+msbuild pump_rods.sln /t:Messages /p:Configuration=Release
 pause
+
